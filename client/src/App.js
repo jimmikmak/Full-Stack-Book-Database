4e9c5caec39730fc1f8fd49f499da09d36310b53
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2021-03-06 15:14:35 +1100

    Login wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import React, { useState } from "react";
 import { BookContainer } from "./components/BookContainer";
 import "./index.css";
+import Login from "./components/Login";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = () => {
+  const [token, setToken] = useState();
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div className="App">
       <BookContainer />
